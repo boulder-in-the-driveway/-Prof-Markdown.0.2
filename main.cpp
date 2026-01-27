a9284e064c6be3d39fe6ddb20ab370541f654543
boulder-in-the-driveway <sheard@carthage.edu>
2026-01-27 19:59:24 +0000

    Test update

--- main.cpp
+++ main.cpp
@@ -53,27 +53,13 @@ TEST_CASE("toItalics Test")
 TEST_CASE("toCode Test")  
 { 
 	string s1 = "`this is code!`";
-	string s2 = "this is not bold";
-	string s3 = "this is not bold **but this is!**";
-	string s4 = "**bold** and **bold**";
-	string s5 = "*this is italics* **and this is bold!**";
-	
+	string s2 = "this is not code";
 
 	MarkdownConverter mc1 = MarkdownConverter(s1);
-	
 	MarkdownConverter mc2 = MarkdownConverter(s2);
-	MarkdownConverter mc3 = MarkdownConverter(s3);
-	MarkdownConverter mc4 = MarkdownConverter(s4);
-	MarkdownConverter mc5 = MarkdownConverter(s5);
 	
-
 	REQUIRE(mc1.toOneLineCode() == "<code>this is code!</code>");
-	
-	REQUIRE(mc2.toBold() == s2); 
-	REQUIRE(mc3.toBold() == "this is not bold <strong>but this is!</strong>");
-	REQUIRE(mc4.toBold() == "<strong>bold</strong> and <strong>bold</strong>");
-	REQUIRE(mc5.toBold() == "*this is italics* <strong>and this is bold!</strong>");
-	
+	REQUIRE(mc2.toOneLineCode() == s2);
 }
 
 
@@ -228,5 +214,5 @@ TEST_CASE("toHors Test")
 
 TEST_CASE ("fileFormatter Test")
 {
-	
+
 }
